04defee80b73812f23652fb6d313ae0e1fb440d7
lucaschm <luca.schmitt@haw-hamburg.de>
2023-12-10 00:53:03 +0100

    add changes to connection_handler

---
+++ chat/src/test/java/de/haw/rn/luca_steven/TestConnectionHandler.java
@@ -0,0 +1,21 @@
+package de.haw.rn.luca_steven;
+
+import java.nio.ByteBuffer;
+
+import org.junit.Test;
+
+public class TestConnectionHandler {
+
+    @Test
+    public void testBuffer() {
+        long number = 0b00000000_00000000_00000000_10011111_01010101_01010101_00000000_11111111L;
+        ByteBuffer headerBuffer = ByteBuffer.allocate(Long.BYTES);
+        headerBuffer.putLong(number);
+
+        int messageLength = headerBuffer.getInt(0);
+        int crc32 = headerBuffer.getInt(1);
+
+        System.out.println(messageLength);
+        System.err.println(crc32);
+    }
+}
