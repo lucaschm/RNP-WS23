2f105e1064c174a4030c9bf9b6728d10ea2dc70a
lucaschm <luca.schmitt@haw-hamburg.de>
2023-12-10 00:53:03 +0100

    add changes to connection_handler

--- chat/src/main/java/de/haw/rn/luca_steven/connection_handler/ConnectionHandler.java
+++ chat/src/main/java/de/haw/rn/luca_steven/connection_handler/ConnectionHandler.java
@@ -1,8 +1,16 @@
 package de.haw.rn.luca_steven.connection_handler;
 import de.haw.rn.luca_steven.data_classes.ChatMessage;
 import javax.json.JsonObject;
+import de.haw.rn.luca_steven.Logger;
 
 public class ConnectionHandler implements IConnectionHandler{
+
+    private int idPort; // Der Port, auf dem der Server hört
+
+    public ConnectionHandler() {
+        idPort = 8080;
+    }
+
     @Override
     public ChatMessage nextMessage() {
         return null;
@@ -20,7 +28,7 @@ public class ConnectionHandler implements IConnectionHandler{
 
     @Override
     public void listen() {
-
+        //thread mit ConnectionListener
     }
 
     @Override
