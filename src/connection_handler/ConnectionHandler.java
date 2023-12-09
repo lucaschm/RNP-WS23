d8a83418030a799b7d9c0803b923bbe020cbd3bc
lucaschm <luca.schmitt@haw-hamburg.de>
2023-12-09 15:14:25 +0100

    first steps of ConnectionHandler

--- src/connection_handler/ConnectionHandler.java
+++ src/connection_handler/ConnectionHandler.java
@@ -1,12 +1,31 @@
 package connection_handler;
 import data_classes.ChatMessage;
 
-public class ConnectionHandler {
-    public ConnectionHandler() {
+import javax.json.JsonObject;
+
+public class ConnectionHandler implements IConnectionHandler{
+    @Override
+    public ChatMessage nextMessage() {
+        return null;
+    }
+
+    @Override
+    public void connect(String ipAddress, int port) {
 
     }
 
-    public ChatMessage nextMessage() {
-        return dummyMessage
+    @Override
+    public void disconnect(String ipAddress, int port) {
+
+    }
+
+    @Override
+    public void listen() {
+
+    }
+
+    @Override
+    public void sendMessage(JsonObject jsonObject) {
+
     }
 }
