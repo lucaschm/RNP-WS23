a7ca47842a5765a8ae9755a5513f6696b4d6378e
lucaschm <luca.schmitt@haw-hamburg.de>
2023-12-10 20:44:14 +0100

    main superloop weitergedacht

--- chat/src/main/java/de/haw/rn/luca_steven/Controller.java
+++ chat/src/main/java/de/haw/rn/luca_steven/Controller.java
@@ -6,11 +6,16 @@ public class Controller {
 
     public static void main(String[] args) {
         ConnectionHandler connectionHandler = new ConnectionHandler(6789);
+        JsonParser jsonParser = new JsonParser();
 
         int i = 0;
         while(true) {
             //Logger.log(6789 + ": main while iteration " + i++);
             connectionHandler.listen();
+
+            // String messageString = connectionHandler.nextMessage();
+            // ChatMessage chatMessage = jsonParser.convertStringToChatMessage(messageString);
+            //TODO: ChatMessage an Router weitergeben
         }
     }
 }
