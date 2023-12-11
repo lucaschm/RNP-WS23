b177f8e4fc10c70f0882b3ff04e02013184cd37c
lucaschm <luca.schmitt@haw-hamburg.de>
2023-12-11 20:20:36 +0100

    update RoutingMessage, so it can return the table as a set of RoutingEntry

--- chat/src/main/java/de/haw/rn/luca_steven/data_classes/RoutingMessage.java
+++ chat/src/main/java/de/haw/rn/luca_steven/data_classes/RoutingMessage.java
@@ -1,12 +1,22 @@
 package de.haw.rn.luca_steven.data_classes;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.json.JsonArray;
 import javax.json.JsonObject;
+import javax.json.JsonValue;
+
+import de.haw.rn.luca_steven.data_classes.routing_table.RoutingEntry;
 
 public class RoutingMessage extends Message {
-    private String ip;
-    private int sourcePort;
-    private int idPort;
+    private static final String IP = "ip";
+    private static final String ID_PORT = "id_port";
+    private static final String HOPS = "hops";
+
+    private String originIP;
+    private int originSourcePort;
+    private int originIDPort;
     private JsonArray table;
 
     public RoutingMessage(String ip,
@@ -14,26 +24,45 @@ public class RoutingMessage extends Message {
                        int idPort,
                        JsonArray table) {
         super(false);
-        this.ip = ip;
-        this.sourcePort = sourcePort;
-        this.idPort = idPort;
+        this.originIP = ip;
+        this.originSourcePort = sourcePort;
+        this.originIDPort = idPort;
         this.table = table;
     }
 
-    public String getIp() {
-        return ip;
+    public String getOriginIP() {
+        return originIP;
     }
 
-    public int getSourcePort() {
-        return sourcePort;
+    public int getOriginSourcePort() {
+        return originSourcePort;
     }
 
-    public int getIdPort() {
-        return idPort;
+    public int getOriginIDPort() {
+        return originIDPort;
     }
 
     public JsonArray getJsonTable() {
         return table;
+    }
+
+    public Set<RoutingEntry> getSetOfRoutingEntries() {
+        Set<RoutingEntry> set = new HashSet<>();
+        for (JsonValue tableEntry : table) {
+            JsonObject jsonObject = tableEntry.asJsonObject();
+            
+            String destinationIP = jsonObject.getString(IP);
+            int destinationIDPort = Integer.parseInt(jsonObject.getString(ID_PORT));
+            int hops = Integer.parseInt(jsonObject.getString(HOPS));
+
+            String destination = destinationIP + ":" + destinationIDPort;
+            hops = hops + 1;
+            String nextHop = originIP + ":" + originIDPort;
+            String origin = nextHop;
+
+            set.add(new RoutingEntry(destination, hops, nextHop, origin));
+        }
+        return set;
     }
 
 }
