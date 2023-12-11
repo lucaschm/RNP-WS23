2ccf6373d8610041397096bc18232be6b0852b07
lucaschm <luca.schmitt@haw-hamburg.de>
2023-12-11 18:56:31 +0100

    upgrade JsonParser, so it can convert Json Strings to both ChatMessage and RoutingMessage

--- chat/src/main/java/de/haw/rn/luca_steven/JsonParser.java
+++ chat/src/main/java/de/haw/rn/luca_steven/JsonParser.java
@@ -6,19 +6,29 @@ import javax.json.*;
 
 import de.haw.rn.luca_steven.data_classes.ChatMessage;
 import de.haw.rn.luca_steven.data_classes.Message;
+import de.haw.rn.luca_steven.data_classes.RoutingMessage;
 
 //bekommt Infos rein (z.b. IP-Adresse)
 //Infos werden verpackt in ein Json Objekt
 
 public class JsonParser {
+
+    //JSON KEYS FOR CHAT MESSAGE
     private static final String MESSAGE_TYPE = "message_type";
     private static final String CHAT_MESSAGE = "chat_message";
     private static final String DESTINATION_IP = "destination_ip";
     private static final String DESTINATION_ID_PORT = "destination_id_port";
     private static final String ORIGIN_IP = "origin_ip";
-    private static final  String ORIGIN_ID_PORT = "origin_id_port";
+    private static final String ORIGIN_ID_PORT = "origin_id_port";
     private static final String TTL = "ttl";
     private static final String CONTENT = "content";
+
+    //JSON KEYS FOR ROUTING MESSAGE
+    private static final String ROUTING_MESSAGE = "routing_message";
+    private static final String IP = "ip";
+    private static final String SOURCE_PORT = "source_port";
+    private static final String ID_PORT = "id_port";
+    private static final String TABLE = "table";
 
     public JsonParser() {
 
@@ -38,22 +48,37 @@ public class JsonParser {
         return json.toString();
     }
 
-    //TODO: anpassen, damit es für beide Arten von Nachrichten funktioniert
     public Message convertJsonStringToMessage(String messageString) throws IllegalArgumentException {
         // String to JSONObject
         JsonReader jsonReader = Json.createReader(new StringReader(messageString));
         JsonObject json = jsonReader.readObject();
-        
-        String testString = json.getString(MESSAGE_TYPE);
 
         // read contents from json
+        Message result = null;
+
+        if (json.getString(MESSAGE_TYPE).equals(CHAT_MESSAGE)) 
+        {
+            result = extractChatMessage(json);
+        } 
+        else if (json.getString(MESSAGE_TYPE).equals(ROUTING_MESSAGE)) 
+        {
+            result = extractRoutingMessage(json);
+        } 
+        else 
+        {
+            throw new IllegalArgumentException("The " + MESSAGE_TYPE + " of the json is neither " + CHAT_MESSAGE + " nor " + ROUTING_MESSAGE + "!");
+        }
+        return result;
+    }
+
+    private ChatMessage extractChatMessage(JsonObject json) {
         String destinationIp = json.getString(DESTINATION_IP);
         int destinationPort = Integer.parseInt(json.getString(DESTINATION_ID_PORT));
         String sourceIp = json.getString(ORIGIN_IP);
         int sourcePort = Integer.parseInt(json.getString(ORIGIN_ID_PORT));
         int ttl = Integer.parseInt(json.getString(TTL));
         String content = json.getString(CONTENT);
-
+        
         return new ChatMessage(
                 destinationIp,
                 destinationPort,
@@ -61,6 +86,15 @@ public class JsonParser {
                 sourcePort,
                 ttl,
                 content);
+    }
+
+    private RoutingMessage extractRoutingMessage(JsonObject json) {
+        String ip = json.getString(IP);
+        int sourcePort = Integer.parseInt(json.getString(SOURCE_PORT));
+        int idPort = Integer.parseInt(json.getString(ID_PORT));
+        JsonArray table = json.getJsonArray(TABLE);
+
+        return new RoutingMessage(ip, sourcePort, idPort, table);
     }
 
 }
--- chat/src/test/java/de/haw/rn/luca_steven/TestJsonParser.java
+++ chat/src/test/java/de/haw/rn/luca_steven/TestJsonParser.java
@@ -7,17 +7,19 @@ import org.junit.Test;
 
 import de.haw.rn.luca_steven.data_classes.ChatMessage;
 import de.haw.rn.luca_steven.data_classes.Message;
+import de.haw.rn.luca_steven.data_classes.RoutingMessage;
 
 public class TestJsonParser {
     private ChatMessage chatMessage;
-    private String jsonString = "{\"message_type\":\"chat_message\",\"destination_ip\":\"192.168.10.3\",\"destination_id_port\":\"12345\",\"origin_ip\":\"192.168.104.12\",\"origin_id_port\":\"9999\",\"ttl\":\"3\",\"content\":\"hello world!\"}";
-        
+    private RoutingMessage routingMessage;
+    private String jsonChatMessage = "{\"message_type\":\"chat_message\",\"destination_ip\":\"192.168.10.3\",\"destination_id_port\":\"12345\",\"origin_ip\":\"192.168.104.12\",\"origin_id_port\":\"9999\",\"ttl\":\"3\",\"content\":\"hello world!\"}";
+    private String jsonRoutingMessage = "{\"message_type\":\"routing_message\",\"ip\":\"172.11.14\",\"source_port\":\"52443\",\"id_port\":\"8081\",\"table\":[{\"ip\":\"172.11.14.11\",\"id_port\":\"8081\",\"hops\":\"0\"},{\"ip\":\"172.11.14.10\",\"id_port\":\"8080\",\"hops\":\"1\"},{\"ip\":\"172.11.14.12\",\"id_port\":\"8080\",\"hops\":\"1\"}]}";
 
-    @Before
-    public void testConvertJsonStringToMessage() {
+    @Before @Test
+    public void testExtractChatMessage() {
         JsonParser jsonParser = new JsonParser();
         
-        Message message = jsonParser.convertJsonStringToMessage(jsonString);
+        Message message = jsonParser.convertJsonStringToMessage(jsonChatMessage);
         assertTrue("Die Nachricht müsste eigentlich eine chat_message sein!", message.isChatMessage());
 
         chatMessage = (ChatMessage) message;
@@ -25,14 +27,24 @@ public class TestJsonParser {
         assertEquals("hello world!", chatMessage.getContent());
     }
 
+    @Before @Test
+    public void testExtractRoutingMessage() {
+        JsonParser jsonParser = new JsonParser();
+
+        Message message = jsonParser.convertJsonStringToMessage(jsonRoutingMessage);
+        assertTrue("Die Nachricht müsste eigentlich eine routing_message sein!", !message.isChatMessage());
+
+        routingMessage = (RoutingMessage) message;
+
+        assertEquals("172.11.14", routingMessage.getIp());
+    }
+
     @Test
-    public void test() {
+    public void testConvertChatMessageToJsonString() {
         JsonParser jsonParser = new JsonParser();
 
         String parsedString = jsonParser.convertChatMessageToJsonString(chatMessage);
 
-        assertEquals(jsonString, parsedString);
-
-
+        assertEquals(jsonChatMessage, parsedString);
     }
 }
