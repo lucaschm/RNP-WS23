1eebdd4c5f7a36d09aea674224e567a71cd6473a
lucaschm <luca.schmitt@haw-hamburg.de>
2023-12-12 12:59:13 +0100

    ui erster Stand

--- chat/src/main/java/de/haw/rn/luca_steven/ui/UI.java
+++ chat/src/main/java/de/haw/rn/luca_steven/ui/UI.java
@@ -1,6 +1,18 @@
 package de.haw.rn.luca_steven.ui;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class UI {
+
+    private BlockingQueue<String> userInputQueue;
+    private BlockingQueue<String> userOutputQueue;
+
+    public UI (BlockingQueue<String> userInputQueue, BlockingQueue<String> userOutputQueue) {
+        this.userInputQueue = userInputQueue;
+        this.userOutputQueue = userOutputQueue;
+    }
 
 //INPUT EINLESEN
     //Befehle:
@@ -9,15 +21,26 @@ public class UI {
         // disconnect from ...
         // alle teilnehmer auflisten
         // exit (Programm schließen)
-    public UserCommand getUserCommand() {
-        return null;
+    public String getUserInput() {
+        synchronized(userInputQueue) {
+            return userInputQueue.poll();
+        }
+        
     }
 
 //OUTPUT AUSGEBEN
 
     // Nachricht von anderem Chatteilnehmer
-    public void printChatMessage() {
-
+    public void printChatMessage(String string) {
+        try {
+            synchronized(userOutputQueue) {
+                userOutputQueue.put(string);
+            }
+            
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
         
     // teilnehmer Liste ausgeben
