4fa65a899a4223fb7dd51ae3f8f7be7235b5775a
lucaschm <luca.schmitt@haw-hamburg.de>
2024-01-06 23:07:56 +0100

    seems like disconnect is working now

--- chat/src/main/java/de/haw/rn/luca_steven/connection_handler/ConnectionHandler.java
+++ chat/src/main/java/de/haw/rn/luca_steven/connection_handler/ConnectionHandler.java
@@ -127,6 +127,11 @@ public class ConnectionHandler implements IConnectionHandler{
 
     @Override
     public boolean disconnect(String ipAddress, int port) {
+        try {
+            selector.selectNow();
+        } catch (IOException e) {
+            Status.unexpectedError("Exception while disconneting: "+ e.getMessage());
+        }
         Set<SelectionKey> selectedKeys = selector.selectedKeys();
         Iterator<SelectionKey> iter = selectedKeys.iterator();
         while (iter.hasNext()) {
